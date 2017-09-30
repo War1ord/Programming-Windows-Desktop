0ca6b30f7a62ea8b86d8ea7d61a4443bf1bd4372
Charles Kuperus <charles.kuperus@ec.co.za>
2017-09-30 21:18:14 +0200

    Initial Commit for Base 64 String File Converter

---
+++ Tools/Base64StringFileConverter/Base64StringFileConverter.API/Extentions/Base64StringConverters.cs
@@ -0,0 +1,24 @@
+﻿namespace Base64StringFileConverter.API
+{
+	/// <summary>
+	/// A class containing all 
+	/// </summary>
+	public static class Base64StringConverters
+	{
+		/// <summary>
+		/// To the content.
+		/// </summary>
+		/// <param name="base64String">The base64 string.</param>
+		/// <returns></returns>
+		/// <exception cref="System.ArgumentException">message - base64String</exception>
+		public static byte[] ToContent(this string base64String)
+		{
+			if (string.IsNullOrWhiteSpace(base64String))
+			{
+				throw new System.ArgumentException("message", nameof(base64String));
+			}
+
+			return new byte[0];
+		}
+	}
+}
