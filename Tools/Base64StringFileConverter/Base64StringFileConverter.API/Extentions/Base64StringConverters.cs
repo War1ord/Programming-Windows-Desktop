88c2555eb00a107ff434cb7a635c14f38d3eacbf
Charles Kuperus <charles.kuperus@ec.co.za>
2017-10-01 17:06:28 +0200

    ...

--- Tools/Base64StringFileConverter/Base64StringFileConverter.API/Extentions/Base64StringConverters.cs
+++ Tools/Base64StringFileConverter/Base64StringFileConverter.API/Extentions/Base64StringConverters.cs
@@ -1,24 +1,43 @@
-﻿namespace Base64StringFileConverter.API
+﻿using System;
+
+namespace Base64StringFileConverter.API
 {
 	/// <summary>
-	/// A class containing all 
+	/// a Class containing all extension methods in the api.
 	/// </summary>
 	public static class Base64StringConverters
 	{
 		/// <summary>
-		/// To the content.
+		/// Converts the base 64 string to content as a byte array.
 		/// </summary>
 		/// <param name="base64String">The base64 string.</param>
-		/// <returns></returns>
-		/// <exception cref="System.ArgumentException">message - base64String</exception>
+		/// <returns>
+		/// The Content of the file as a byte array
+		/// </returns>
+		/// <exception cref="ArgumentNullException">base64String</exception>
 		public static byte[] ToContent(this string base64String)
 		{
 			if (string.IsNullOrWhiteSpace(base64String))
-			{
-				throw new System.ArgumentException("message", nameof(base64String));
-			}
-
-			return new byte[0];
+				throw new ArgumentNullException(nameof(base64String));
+			else
+				return Convert.FromBase64String(base64String);
 		}
+
+		/// <summary>
+		/// Converts the byte array to content as a base 64 string.
+		/// </summary>
+		/// <param name="fileContent">Content of the file.</param>
+		/// <returns>
+		/// The Content of the file as a base 64 string.
+		/// </returns>
+		/// <exception cref="ArgumentNullException">fileContent</exception>
+		public static string ToBase64String(this byte[] fileContent)
+		{
+			if (fileContent == null)
+				throw new ArgumentNullException(nameof(fileContent));
+			else
+				return Convert.ToBase64String(fileContent);
+		}
+
 	}
 }
